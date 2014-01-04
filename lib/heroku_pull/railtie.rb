120f1740714cdda4c16fb73bcc15370e49ab8494
David Padilla <david@crowdint.com>
2014-01-03 21:16:10 -0600

    Set the target Heroku app with APP env

--- lib/heroku_pull/railtie.rb
+++ lib/heroku_pull/railtie.rb
@@ -6,6 +6,7 @@ module HerokuPull
       namespace :heroku do
         desc "Pull the Postgres database from Heroku into local server"
         task :pull do
+          HerokuPull.app_name = ENV['APP']
           HerokuPull.capture
           HerokuPull.download
           HerokuPull.restore
